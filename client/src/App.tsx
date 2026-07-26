585d9dc363a2960af4ddbdb52fd03049c60ab9ee
Aliaksandr Torkhau <torkhau@gmail.com>
2026-07-26 19:30:33 +0200

    feat(app): wrap main components with CapacityProvider for improved state management

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
 import { Separator } from './components/separator';
 import { TokenProvider } from './contexts/token';
+import { CapacityProvider } from './contexts/userData';
 import { Header } from './features/header';
 import { Main } from './features/main';
 
 export function App() {
   return (
     <TokenProvider>
-      <Header />
-      <Separator />
-      <Main />
+      <CapacityProvider>
+        <Header />
+        <Separator />
+        <Main />
+      </CapacityProvider>
     </TokenProvider>
   );
 }
