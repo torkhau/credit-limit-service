30e07d4d8fc89f38def22a3d5be37ae75986409f
Aliaksandr Torkhau <torkhau@gmail.com>
2026-07-26 14:57:42 +0200

    feat(currency): add currency constants and types for USD, EUR, GBP, and PLN

---
+++ src/common/constants/currency.constant.ts
@@ -0,0 +1,6 @@
+export const CURRENCY = {
+  USD: 1,
+  EUR: 0.85,
+  GBP: 0.75,
+  PLN: 3.8,
+};
---
+++ src/common/types/currency.type.ts
@@ -0,0 +1,3 @@
+import { CURRENCY } from '../constants/currency.constant';
+
+export type Currency = keyof typeof CURRENCY;
