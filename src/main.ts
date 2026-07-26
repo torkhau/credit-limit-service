2d569f33fc4faf1af65ce809faee1704c57fe1fc
Aliaksandr Torkhau <torkhau@gmail.com>
2026-07-26 12:23:52 +0200

    refactor(main): update global API prefix and enable versioning

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
 
   await app.listen(3000);
 }
